4cde1263b0b3e6e3d02b3aa91277e5cf9ea5ccc9
akp28 <ajaypaladugu@gmail.com>
2018-10-06 14:31:57 -0700

    cosmetic changes

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,4 @@
-var message = "Guess The Letter From a (lower) to z (higher)";
+var message = "Guess The Letter From a to z";
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var triesLeft = 9;
 var lettersGuessed = [];
